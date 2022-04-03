0a82bedcb9ef71af8baf40359af2ff5a05d4ea91
creemy0cheesekake <arjunanand1405@gmail.com>
2022-04-03 04:40:10 -0500

    ^package.json

--- game.ts
+++ game.ts
@@ -1,48 +1,53 @@
 import GameInit from "./gameinit";
-import argsParser from "args-parser";
 import Settings from "./settings";
+import argsParser from "args-parser";
 import * as types from "./types";
 
 const { boardWidth, boardHeight } = Settings.gameSettings;
 const BORDER_CHAR = "X";
+const SNAKE_CHAR = "O";
 
 class Game {
-	board: any[][];
-	snakeSize: number = 3;
+	board: string[][];
 	snakePos: types.TSnakeCoords = [~~(boardWidth / 2), ~~(boardHeight / 2)];
-	snakeTurnPoints: types.TSnakeCoords[] = [];
+	snakeLen: number = 1;
 	snakeDir: types.TSnakeDir = "left";
 	constructor() {
-		new GameInit(argsParser(process.argv));
-		console.log(this.snakePos);
+		const status = new GameInit(argsParser(process.argv)).handleParams();
+		!status && process.exit(0);
 		this.board = Array(boardHeight)
 			.fill("")
 			.map((_, i) =>
 				i === 0 || i === boardHeight - 1
 					? Array(boardWidth).fill(BORDER_CHAR)
-					: Array(boardWidth).fill(" ")
+					: Array(boardWidth)
+							.fill("")
+							.map((_, i) => (i === 0 || i === boardWidth - 1 ? BORDER_CHAR : " "))
 			);
-		// Only horizontal borders are drawn here, vertical borders will be added when its drawn.
 	}
 	main() {
-		this.drawBoard();
+		this.displayBoard();
 	}
-	drawBoard() {
-		this.updateSnake();
+	displayBoard() {
+		console.clear();
+		this.drawSnake();
 		for (let row of this.board) {
-			// Draw vertical borders.
-			row[0] = BORDER_CHAR;
-			row[boardWidth - 1] = BORDER_CHAR;
 			let temp = "";
-			for (let square of row) {
-				temp += `${square} `;
-			}
+			for (let i of row) temp += `${i} `;
 			console.log(temp.trim());
 		}
 	}
-	updateSnake() {
-		// putting 1 before 0 so that snakePos can be represeted as [x, y], not [y, x]
-		this.board[this.snakePos[1]][this.snakePos[0]] = "O";
+	drawSnake() {
+		console.log(this.snakePos);
+		this.board[this.snakePos[1]][this.snakePos[0]] = SNAKE_CHAR;
+		this.moveSnake();
+	}
+	moveSnake() {
+		if (this.snakeDir === "left" || this.snakeDir === "right")
+			this.snakePos[1] += this.snakeDir === "right" ? 1 : -1;
+		if (this.snakeDir === "up" || this.snakeDir === "down")
+			this.snakePos[1] += this.snakeDir === "down" ? 1 : -1;
+		this.displayBoard();
 	}
 }
 
